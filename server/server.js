738af244d8648966d652c4892249189b56d1eb69
Andrew Lilley <andrew.c.lilley@gmail.com>
2020-06-17 13:58:05 +0100

    Add compression for express, missing from the build

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const compression = require('compression')
+const compression = require('compression');
 const express = require('express');
 const app = express(compression());
 const publicPath = path.join(__dirname, '..', 'public');
